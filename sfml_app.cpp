45778de88c23df99c94157642c005ed60da98f8e
Jintao Huang <57041780+Galladerotom@users.noreply.github.com>
2021-06-05 14:31:58 +0200

    First commit. Added basic board, pieces

---
+++ sfml_app.cpp
@@ -0,0 +1,109 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+
+// chess piece size
+int size = 60;
+std::string position = "";
+
+// board initual layout
+int board[8][8] =
+{ -3,-4,-5,-2,-1,-5,-4,-3,
+ -6,-6,-6,-6,-6,-6,-6,-6,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  6, 6, 6, 6, 6, 6, 6, 6,
+  3, 4, 5, 2, 1, 5, 4, 3 };
+// chess pieces
+Sprite f[32];
+
+//functions
+
+void loadPosition()
+{
+    int k = 0;
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+        {
+            int n = board[i][j];
+            if (!n) continue;
+            int x = abs(n) - 1;
+            int y = n > 0 ? 1 : 0;
+            f[k].setTextureRect(IntRect(size * x, size * y, size, size));
+            f[k].setPosition(size * j, size * i);
+            k++;
+        }
+
+    for (int i = 0; i < position.length(); i += 5)
+        move(position.substr(i, 4));
+}
+
+// draw blank chess board
+void drawBoard(RenderWindow & window) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            auto colour = ((i + j) % 2 == 1) ? Color(118, 150, 86) : Color(238, 238, 210);
+            sf::RectangleShape rectangle(Vector2f(60,60));
+            rectangle.setFillColor(colour);
+            rectangle.setPosition(60 * i, 60 * j);
+            window.draw(rectangle);
+        }
+    }
+}
+Vector2f toCoord(char a, char b)
+{
+    int x = int(a) - 97;
+    int y = 7 - int(b) + 49;
+    return Vector2f(x * size, y * size);
+}
+void move(std::string str)
+{
+    Vector2f oldPos = toCoord(str[0], str[1]);
+    Vector2f newPos = toCoord(str[2], str[3]);
+
+    for (int i = 0; i < 32; i++)
+        if (f[i].getPosition() == newPos) f[i].setPosition(-100, -100);
+
+    for (int i = 0; i < 32; i++)
+        if (f[i].getPosition() == oldPos) f[i].setPosition(newPos);
+
+    //castling       //if the king didn't move
+    if (str == "e1g1") if (position.find("e1") == -1) move("h1f1");
+    if (str == "e8g8") if (position.find("e8") == -1) move("h8f8");
+    if (str == "e1c1") if (position.find("e1") == -1) move("a1d1");
+    if (str == "e8c8") if (position.find("e8") == -1) move("a8d8");
+}
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(480, 480), "SFML works!");
+    Texture pieces;
+    pieces.loadFromFile("images/pieces.png");
+    for (int i = 0; i < 32; i++) f[i].setTexture(pieces);
+    loadPosition();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+                std::cout << localPosition.x<< " " << localPosition.y << std::endl;
+            }
+
+        }
+
+        window.clear();
+        drawBoard(window);
+        for (int i = 0; i < 32; i++) window.draw(f[i]); window.draw(f[1]);
+        window.display();
+    }
+
+    return 0;
+}
